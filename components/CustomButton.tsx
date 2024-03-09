0afe37b0dae8ca761c3446555ba066c8ae7ab3b1
damirpavlik <damir.pavlik@gmail.com>
2024-03-09 13:47:57 +0100

    feat: adding props to the main button components

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -2,15 +2,33 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType,
+  textStyles,
+  rightIcon,
+  isDisabled,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            fill
+            className="object-contain"
+            alt="right button icon"
+          />
+        </div>
+      )}
     </button>
   );
 };
